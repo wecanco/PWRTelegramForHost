b3cd085186b737a85c1b95781461ee5ac30efbca
WeCan Group <mutalebe@live.com>
2018-04-07 21:23:07 +0430

    update

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+	if(!file_exists('timeout')){
+		mkdir('timeout');
+	}
+	if(!file_exists('users')){
+		mkdir('users');
+	}
+	
+	if(!file_exists('sessions')){
+		mkdir('sessions');
+	}
+				
 	if(!file_exists('__conf.php')){
 		header('location: /installer.php');
 	}else{
